f38c9739606fc6107eca95327b11af4d5b898037
Anwesh43 <anweshthecool0@gmail.com>
2020-11-25 03:31:02 +0530

    adding DiagonalBlockLadderView class

--- DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
+++ DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
@@ -49,3 +49,19 @@ fun Canvas.drawDBLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawDiagonalBlockLadder(scale, w, h, paint)
 }
+
+class DiagonalBlockLadderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
