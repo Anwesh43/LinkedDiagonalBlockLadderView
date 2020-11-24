83faf19ed8ffffd3275d86b9e647fb314fdf5b6a
Anwesh43 <anweshthecool0@gmail.com>
2020-11-25 03:21:41 +0530

    adding Canvas extension methods

--- DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
+++ DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
@@ -28,3 +28,24 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawDiagonalBlockLadder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = (w / 2) / (blocks - 0.5f)
+    val sf : Float = scale.sinify()
+    for (j in 0..(blocks - 1)) {
+        save()
+        translate(
+            w / 2 - size * j * (1 - sf.divideScale(blocks, parts)),
+            h - (size * (j + 1) * sf.divideScale(j, parts))
+        )
+        drawRect(RectF(-size / 2, -size / 2, size / 2, size/ 2), paint)
+        restore()
+    }
+}
+
+fun Canvas.drawDBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawDiagonalBlockLadder(scale, w, h, paint)
+}
