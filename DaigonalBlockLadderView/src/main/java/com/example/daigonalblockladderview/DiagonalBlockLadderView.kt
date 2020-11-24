8e64bd2e277748fbfc9803e2973cece50e873613
Anwesh43 <anweshthecool0@gmail.com>
2020-11-25 03:05:15 +0530

    adding constants and libraries

--- DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
+++ DaigonalBlockLadderView/src/main/java/com/example/daigonalblockladderview/DiagonalBlockLadderView.kt
@@ -1,2 +1,25 @@
 package com.example.daigonalblockladderview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val blocks : Int = 5
+val parts : Int = 7
+val scGap : Float = 0.02f / parts
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#3F51B5",
+    "#4CAF50",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
